dfeccd0e30c78031bcf252794c17a03969203220
Ariel Dorssi <adorssi@gmail.com>
2022-04-07 20:23:12 -0300

    FeaturedProperties and PropertyCard were modified adding properties from DB

--- src/components/FeaturedProperties/FeaturedProperties.js
+++ src/components/FeaturedProperties/FeaturedProperties.js
@@ -1,97 +1,67 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import PropertyCard from '../PropertyCard/PropertyCard';
 import './FeaturedProperties.css';
 
 function FeaturedProperties() {
 
-  let properties = [
-    {
-      title: 'Hermosa propiedad a estrenar',
-      city: 'Nueva Palmira',
-      operation: 'Venta',
-      ref: 1236,
-      mainImage: '../../images/bg-home.jpg',
-      imageGallery: ['../../images/p1.jpg','../../images/p2.jpg','../../images/p3.jpg','../../images/p4.jpg'],
-      bedrooms: '2',
-      toilet: '1',
-      garage: '1',
-      currency: 'U$S',
-      price: 55000,
-      financing: true,
-    },
-    {
-      title: 'Apartamento frente al rio',
-      city: 'Carmelo',
-      operation: 'Alquiler',
-      ref: 1636,
-      mainImage: '../../images/bg-home.jpg',
-      imageGallery: ['../../images/p1.jpg','../../images/p2.jpg','../../images/p3.jpg','../../images/p4.jpg'],
-      bedrooms: '3',
-      toilet: '1',
-      garage: '1',
-      currency: '$',
-      price: 15000,
-      financing: false,
-    },
-    {
-      title: 'Casa con amplio terreno',
-      city: 'Agraciada',
-      operation: 'Venta',
-      ref: 1136,
-      mainImage: '../../images/bg-home.jpg',
-      imageGallery: ['../../images/p1.jpg','../../images/p2.jpg','../../images/p3.jpg','../../images/p4.jpg'],
-      bedrooms: '4',
-      toilet: '2',
-      garage: '1',
-      currency: 'U$',
-      price: 155000,
-      financing: false,
-    },
-    {
-      title: 'Chara de 12ha. productiva',
-      city: 'Rural',
-      operation: 'Venta',
-      ref: 1238,
-      mainImage: '../../images/bg-home.jpg',
-      imageGallery: ['../../images/p1.jpg','../../images/p2.jpg','../../images/p3.jpg','../../images/p4.jpg'],
-      bedrooms: '0',
-      toilet: '0',
-      garage: '0',
-      currency: 'U$S',
-      price: 260000,
-      financing: false,
-    }
-  ]
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [properties, setProperties] = useState([]);
+
+  const url = `${process.env.REACT_APP_API_URL}/api/property/featured`;
+
+	useEffect(() => {
+		  fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            setIsLoaded(true);
+            setProperties(data.data);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
+	}, []);
+
+  if (error) {
+    return <div>Error: {error.message} </div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
   return (
     <div>
       <div className='featured__title'>
         <h2>Destacadas</h2>
       </div>
       <div className='properties__container'>
-      <PropertyCard />
-        {/* {
+
+        {
           properties.map((property, i) => 
             <PropertyCard
               key= {property + i}
+              id= {property.id}
               title= {property.title}
-              city= {property.city}
-              operation= {property.operation}
+              city= {property.city.name}
+              operation= {property.operationType}
               refer= {property.ref}
-              mainImage= {property.mainImage}
-              imageGallery= {property.imageGallery}
+              // mainImage= {property.mainImage}
+              // imageGallery= {property.imageGallery}
               bedrooms= {property.bedrooms}
-              toilet= {property.toilet}
+              toilet= {property.toilets}
               garage= {property.garage}
-              currency= {property.currency}
+              currency= {property.currency.symbol}
               price= {property.price}
+              showPrice= {property.showPrice}
               financing= {property.financing}
             />
           )
-        }  */}
+        } 
 
       </div>
     </div>
-  )
+  )}
 }
 
 export default FeaturedProperties;
--- src/components/PropertyCard/PropertyCard.js
+++ src/components/PropertyCard/PropertyCard.js
@@ -1,84 +1,72 @@
 import React from 'react';
 import './PropertyCard.css';
-import { LocationMarkerIcon } from "@heroicons/react/outline";
-import { TagIcon, ArrowCircleLeftIcon, ArrowCircleRightIcon } from "@heroicons/react/outline";
+import { TagIcon, ArrowCircleLeftIcon, ArrowCircleRightIcon, LocationMarkerIcon } from "@heroicons/react/outline";
 import bgHome from '../../images/bg-home.jpg';
 import { useState, useEffect } from 'react';
 
 
 function PropertyCard(props) {
-    let id = 3;
-    const url = `${process.env.REACT_APP_API_URL}/api/property/${id}`;
-    const [property, setProperty] = useState([])
+    
+    const { title, operationType, refer, mainImage, imageGallery, bedrooms, toilets, garage, price, financing, featured, city, operation, currency, showPrice } = props;
 
-	useEffect(() => {
-		fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			setProperty(data.data);
-		});
-	}, []);
-
-    const { title, operationType, ref, mainImage, imageGallery, bedrooms, toilet, garage, price, financing, featured } = property;
-
-  return (
-    <div className='property__card'>
-        <div className='property__card-images'>
-            <div className='gallery'>
-                <div className='gallery__buttons'>
-                    <span className='arrow arrow--left'> <ArrowCircleLeftIcon /></span>
-                    <span className='arrow arrow--right'><ArrowCircleRightIcon /></span>
+        return (
+            <div className='property__card'>
+                <div className='property__card-images'>
+                    <div className='gallery'>
+                        <div className='gallery__buttons'>
+                            <span className='arrow arrow--left'> <ArrowCircleLeftIcon /></span>
+                            <span className='arrow arrow--right'><ArrowCircleRightIcon /></span>
+                        </div>
+                        
+                        <img className='gallery-item' src={bgHome} width='100%' alt='Imagen Propiedad' />
+                    
+                        {/* {
+                            imageGallery.map(image => {
+                                return <img src={image} width='100%' alt='Imagen Propiedad' />
+                            })
+                        }                  */}
+                    </div>
                 </div>
-                
-                <img className='gallery-item' src={bgHome} width='100%' alt='Imagen Propiedad' />
-            
-                {/* {
-                    imageGallery.map(image => {
-                        return <img src={image} width='100%' alt='Imagen Propiedad' />
-                    })
-                }                  */}
-            </div>
-        </div>
-        <div className='property__card-info'>
-            <div className='property__card-tags'>
-                <div className='location tag'>
-                    <LocationMarkerIcon className='location-icon' />
-                    <p>{ property.city.name }</p>
+                <div className='property__card-info'>
+                    <div className='property__card-tags'>
+                        <div className='location tag'>
+                            <LocationMarkerIcon className='location-icon' />
+                            <p>{ city }</p>
+                        </div>
+                        <div className='operation tag'>
+                            <TagIcon className='operation-icon' />
+                            <p>{ operation }</p>
+                        </div>
+                    </div>
+                    <div className='title'>
+                            <span className='ref tag'>REF# { refer }</span>
+                            <h2 className='property-title'>{title}</h2>
+                    </div>
+                    <section className='main-amenities'>
+                        <article>
+                            <i className="fas fa-bed"></i>
+                            <p>{ bedrooms }</p>
+                        </article>
+                        <article>
+                            <i className="fas fa-toilet"></i>
+                            <p>{ toilets }</p>
+                        </article>
+                        <article>
+                            <i className="fas fa-car"></i>
+                            <p>{ garage }</p>
+                        </article>
+                    </section>
+                    <section className='end-items'>
+                        { financing && <span className='financing'>¡Financiación!</span>}
+                        
+                        <article className='price-fields'>
+                           { showPrice ? <span className='price'> { currency } { parseInt(price) } </span> : <span className='price'>Consultar</span> }
+                        </article>
+                        
+                    </section>
                 </div>
-                <div className='operation tag'>
-                    <TagIcon className='operation-icon' />
-                    <p>{ operationType }</p>
-                </div>
             </div>
-            <div className='title'>
-                    <span className='ref tag'>REF# { ref }</span>
-                    <h2 className='property-title'>{title}</h2>
-            </div>
-            <section className='main-amenities'>
-                <article>
-                    <i className="fas fa-bed"></i>
-                    <p>{ bedrooms }</p>
-                </article>
-                <article>
-                    <i className="fas fa-toilet"></i>
-                    <p>{ toilet }</p>
-                </article>
-                <article>
-                    <i className="fas fa-car"></i>
-                    <p>{ garage }</p>
-                </article>
-            </section>
-            <section className='end-items'>
-                { financing && <span className='financing'>¡Financiación!</span>}
-                
-                <article className='price-fields'>
-                    <span className='price'> { property.currency.symbol } { price }</span>
-                </article>
-                
-            </section>
-        </div>
-    </div>
-  )
+        )
 }
 
 export default PropertyCard;
